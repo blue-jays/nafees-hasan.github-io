a5c793af963978f618c5c86b4a19d9d5c91e5679
Md Nazmun Hasan Nafees <nafeeshasan365@gmail.com>
2025-11-18 12:16:08 -0500

    Feature: Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,6 +22,7 @@ const Navbar = () => {
     { name: 'About', href: '#about' },
     { name: 'Projects', href: '#projects' },
     { name: 'Data Analysis', href: '#data-analysis' },
+    { name: 'Statistical Analysis', href: '#statistical-analysis' },
     { name: 'MLOps', href: '#mlops' },
     { name: 'Resume', href: '#resume' },
     { name: 'Contact', href: '#contact' },
